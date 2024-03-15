5060a4014aaf58b16aff140a24bd3beb7390131b
Fabian Mendez <ofmendez@gmail.com>
2024-03-14 22:26:05 -0500

    Fix import statement in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Iframe } from 'react-iframe';
+import Iframe  from 'react-iframe';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
