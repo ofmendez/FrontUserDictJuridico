db8896fd6ba79c9fd4e3d367a5c69e7518d09348
Andres Garcia <andresgarcia@tretto.co>
2024-04-02 17:38:14 -0500

    ContenedorScroll Resultados

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -4,33 +4,36 @@ const SearchResults = ({ results }) => {
 	console.log('SearchResults', results);
 	return (
 		<div className='ContenedorDefiniciones'>
+
 			<h3>Resultados</h3>
-			{results.length === 0 && <><br /><p>No se encontraron resultados</p></>}
-			<br />
-			{results.map((r, i) =>
-				<div key={i}>
+			<div className='ContenedorResultadosScroll ScrollVerde'>
+				{results.length === 0 && <><br /><p>No se encontraron resultados</p></>}
+				<br />
+				{results.map((r, i) =>
+					<div key={i}>
 
-					<Link to={`/terms/${r._id}`} className='ResultadoBusqueda'>
-						<div className='ResultadoBusqueda hovered'>
-							<p className='ResultadoBusquedaTermino'>
-								{r.term}
-							</p>
-							<p className='ResultadoBusqueda'>
-								{
-									r.highlights.reduce((max, tmp) => max.score > tmp.score ? max : tmp).texts.map((t, i) => {
-										if (t.type === 'text')
-											return <span key={i}> {t.value}</span>;
-										else
-											return <mark key={i}> {t.value} </mark>;
-									})
-								}
-							</p>
-						</div>
+						<Link to={`/terms/${r._id}`} className='ResultadoBusqueda'>
+							<div className='ResultadoBusqueda hovered'>
+								<p className='ResultadoBusquedaTermino'>
+									{r.term}
+								</p>
+								<p className='ResultadoBusqueda'>
+									{
+										r.highlights.reduce((max, tmp) => max.score > tmp.score ? max : tmp).texts.map((t, i) => {
+											if (t.type === 'text')
+												return <span key={i}> {t.value}</span>;
+											else
+												return <mark key={i}> {t.value} </mark>;
+										})
+									}
+								</p>
+							</div>
 
-					</Link>
-					<hr className='ResultadoBusquedaSeparador' />
-				</div>
-			)}
+						</Link>
+						<hr className='ResultadoBusquedaSeparador' />
+					</div>
+				)}
+			</div>
 		</div>
 	);
 };
