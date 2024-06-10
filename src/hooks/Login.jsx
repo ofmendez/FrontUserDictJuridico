86d49c92b728a73057b27a31821605364a1bb5ac
Fabian Mendez <ofmendez@gmail.com>
2024-06-09 22:00:22 -0500

    feat: Improve error handling in Login.jsx
    The code changes in Login.jsx improve the error handling by properly logging and displaying error messages when there is an error during the login process.

--- src/hooks/Login.jsx
+++ src/hooks/Login.jsx
@@ -1,11 +1,11 @@
 const tryLogin = ({ loadingFetch, setLoadingFetch, query, handleDoneFetch }) => {
 	if (loadingFetch === 'ok') return;
 	fetchLogin({ setLoadinng: setLoadingFetch, path: '/login', query }).then((d) => {
-		handleDoneFetch(d)
+		handleDoneFetch(d);
 		console.log('done fetch: ', d);
 	})
 		.catch((err) => {
-			console.log('the error: ',err);
+			console.log('the error: ', err);
 			window.alert(err);
 		});
 };
@@ -14,7 +14,6 @@ const fetchLogin = ({ setLoadinng, path, query }) => {
 	return new Promise((resolve, reject) => {
 		setLoadinng('loading');
 		const uri = import.meta.env.VITE_API_URI;
-
 		fetch(`${uri}${path}?${new URLSearchParams(query)}`).then(async res => {
 			if (res.ok) return res.json();
 			const text = await res.text();
