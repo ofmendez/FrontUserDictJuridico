8ffe4ed6642da7849c2524c13331742456a152b4
Andres Garcia <andresgarcia@tretto.co>
2024-07-16 10:25:09 -0500

    Correccion clase botones

--- src/pages/Terms.jsx
+++ src/pages/Terms.jsx
@@ -57,8 +57,8 @@ const Terms = () => {
 						</Link>
 					</form>
 					<div className='ContenedorBotonesExpandirContraer'>
-						<Button className='BotonExpandir' onClick={expandAllRows}><span>+</span></Button>
-						<Button className='BotonContraer' onClick={collapseAllRows}><span>-</span></Button>
+						<Button className={'BotonExpandir'} onClick={expandAllRows}><span>+</span></Button>
+						<Button className={'BotonContraer'} onClick={collapseAllRows}><span>-</span></Button>
 					</div>
 				</div>
 				<div className='SeccionSuperiorHerramientas' id='SeccionMensajeEstadisticas' />
